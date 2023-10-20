54388a8999d083b756debecc8f58cd1a8686383c
cookiestealer3114 <yarmakgleb@gmail.com>
2023-10-20 22:18:56 +0600

    Almost finished stats tabs

--- script.js
+++ script.js
@@ -1,47 +1,51 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const statsContainer = document.getElementById("statsContainer");
+  const stats_grid_items = document.querySelectorAll(".stats_grid_item");
 
-  // Загрузите данные из JSON файла
   fetch("legends.json")
     .then((response) => response.json())
     .then((data) => {
-      // Группируйте данные по классам
-      const groupedData = {};
-      data.forEach((item) => {
-        if (!groupedData[item.group]) {
-          groupedData[item.group] = [];
+      data.forEach((item, index) => {
+        const stats_grid_item = stats_grid_items[index];
+
+        const content = document.createElement("div");
+        content.className = "content";
+
+        const valueElement = document.createElement("div");
+        valueElement.className = "value";
+        valueElement.textContent =
+          item.value >= 1000
+            ? (item.value / 1000).toFixed(1) + "K"
+            : item.value;
+
+        const titleElement = document.createElement("div");
+        titleElement.className = "title";
+        titleElement.textContent = item.title;
+
+        content.appendChild(valueElement);
+        content.appendChild(titleElement);
+
+        const isRiseElement = document.createElement("div");
+        isRiseElement.className = item.isRise ? "isRise" : "isRise is-fall";
+
+        const arrowImage = document.createElement("img");
+        arrowImage.src = item.isRise
+          ? "./img/green-arrow.svg"
+          : "./img/yellow-arrow.svg";
+
+        isRiseElement.appendChild(arrowImage);
+
+        const differenceElement = document.createElement("div");
+        differenceElement.className = "difference";
+        differenceElement.textContent = item.difference + "%";
+
+        if (!item.isRise) {
+          differenceElement.classList.add("yellow");
         }
-        groupedData[item.group].push(item);
+
+        stats_grid_item.appendChild(content);
+        stats_grid_item.appendChild(isRiseElement);
+        stats_grid_item.appendChild(differenceElement);
       });
-
-      // Создайте элементы для каждой группы и присвойте классы
-      for (const group in groupedData) {
-        const groupElement = document.createElement("div");
-        groupElement.className = group; // Присвоение класса
-        groupedData[group].forEach((item) => {
-          const statElement = document.createElement("div");
-
-          // Функция для форматирования значения с добавлением знака процента
-          const formatDifference = (value) => {
-            return value + "%";
-          };
-
-          statElement.innerHTML = `
-              <p>Title: ${item.title}</p>
-              <p>Value: ${
-                item.value >= 1000
-                  ? (item.value / 1000).toFixed(1) + "K"
-                  : item.value
-              }</p>
-              <p>Is Rise: ${item.isRise ? "True" : "False"}</p>
-              <p>Difference: ${formatDifference(item.difference)}</p>
-          `;
-          groupElement.appendChild(statElement);
-        });
-        statsContainer.appendChild(groupElement);
-      }
     })
-    .catch((error) =>
-      console.error("Произошла ошибка при загрузке JSON файла:", error)
-    );
+    .catch((error) => console.error("JSON LOADING ERROR", error));
 });
