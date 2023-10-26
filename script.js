ef8d82f57406f73c9fd27125a3674816e1dd95f7
cookiestealer3114 <yarmakgleb@gmail.com>
2023-10-27 00:42:38 +0600

    Finished Visitors Analytics chart

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Код для stats блоков
 document.addEventListener("DOMContentLoaded", function () {
   const stats_grid = document.querySelector(".stats_grid");
 
@@ -57,6 +58,129 @@ document.addEventListener("DOMContentLoaded", function () {
         stats_grid_item.appendChild(risedif);
 
         stats_grid.appendChild(stats_grid_item);
+      });
+    })
+    .catch((error) => console.error("JSON LOADING ERROR", error));
+});
+// chartAreaBorder - плагин для бордера
+// const chartAreaBorder = {
+//   id: "chartAreaBorder",
+//   beforeDraw(chart, args, options) {
+//     const {
+//       ctx,
+//       chartArea: { left, top, width, height },
+//     } = chart;
+//     ctx.save();
+//     ctx.strokeStyle = options.borderColor;
+//     ctx.lineWidth = options.borderWidth;
+//     ctx.setLineDash(options.borderDash || []);
+//     ctx.lineDashOffset = options.borderDashOffset;
+//     ctx.strokeRect(left, top, width, height);
+//     ctx.restore();
+//   },
+// };
+// Код для графика Visitors Analytics
+document.addEventListener("DOMContentLoaded", function () {
+  fetch("analytics.json")
+    .then((response) => response.json())
+    .then((data) => {
+      const days = data.days.map((entry) => entry.day);
+      const values = data.days.map((entry) => entry.value);
+      const maxValue = Math.max(...values);
+      const yAxisMax = Math.ceil(maxValue / 100) * 100;
+
+      const canvasElements = document.querySelectorAll(".myChart");
+
+      canvasElements.forEach((canvas) => {
+        const ctx = canvas.getContext("2d");
+
+        new Chart(ctx, {
+          // plugins: [chartAreaBorder],
+          // Включает плагин
+          type: "bar",
+          data: {
+            labels: days,
+            datasets: [
+              {
+                label: "Значение",
+                data: values,
+                backgroundColor: "#3C50E0",
+                borderWidth: 2,
+                borderRadius: 15,
+              },
+            ],
+          },
+          options: {
+            maintainAspectRatio: false,
+            plugins: {
+              legend: {
+                display: false,
+              },
+              // chartAreaBorder: {
+              //   borderWidth: 1,
+              //   borderDash: [5, 5],
+              //   borderDashOffset: 2,
+              // },
+              // Задает значения бордера
+            },
+            scales: {
+              x: {
+                type: "linear",
+                position: "bottom",
+                ticks: {
+                  stepSize: 1,
+                  font: {
+                    color: "#64748b",
+                    size: 14,
+                    lineHeight: 2.2,
+                    family: "Poppins",
+                    style: "normal",
+                    weight: 500,
+                  },
+                },
+                grid: {
+                  display: false,
+                  color: "#E2E8F0",
+                  offset: true,
+                  drawBorder: false,
+                },
+              },
+              y: {
+                ticks: {
+                  min: 0,
+                  max: yAxisMax,
+                  stepSize: yAxisMax / 4,
+                  callback: function (value, index, values) {
+                    return value.toFixed(0);
+                  },
+                  callback: function (value, index) {
+                    return " " + value;
+                  },
+                  font: {
+                    color: "#64748b",
+                    size: 14,
+                    lineHeight: 2.2,
+                    family: "Poppins",
+                    style: "normal",
+                    weight: 500,
+                    align: "right",
+                  },
+                },
+                grid: {
+                  display: true,
+                  drawBorder: false,
+                  color: "#E2E8F0",
+                },
+
+                border: {
+                  dash: [10, 5],
+                },
+              },
+            },
+          },
+        });
+
+        // canvas.style.height = "500px";
       });
     })
     .catch((error) => console.error("JSON LOADING ERROR", error));
